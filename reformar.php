131427a6785e1ef4dbb83ffd6ca987e417c818ee
Carlos Nascimento <carlosnascimentophp@gmail.com>
2025-01-28 14:15:30 -0300

    Create reformar.php

---
+++ reformar.php
@@ -0,0 +1,104 @@
+
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- icones fontawase--->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
+    <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <!-- CSS Externos --->
+    <link rel="stylesheet" href="Css/styles.css"/>
+    <link rel="stylesheet" href="Css/media_query_computer.css"/>
+          
+    <title>CN::Carlos Nascimento</title>
+  </head>
+  <body>
+
+    <header>
+      <nav class="navbar bg-body-tertiary fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <i class="fa-brands fa-java"></i>
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">
+            <i class="fa-brands fa-java"></i>
+        </h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Quem somos</a>
+          </li>
+           
+        </ul>
+        <form class="d-flex mt-3" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</nav>
+  
+    </header>
+
+    <section class="start">
+      <div class="container">
+          <div class="hero-content px-1">
+              <div class="hero text-default-color">
+                    <h1 class="text-gradient">COFFEO </h1>
+                    <h2>Store online de café</h2>
+                    <p> Descubra o Sabor de cada momento com COFFEO. Sua vitrine online sobre café e sabores!</p>
+                    <button class="btn-marrom"> Explore os produtos!</button>
+
+                <div class="group-mini-cards">
+              
+              <div class="mini-card text-gradient">
+                <h2>Tipos de café</h2>
+                <p>+ de 20</p>
+              </div>
+
+              <div class="mini-card text-gradient">
+                <h2>Espécies do grão</h2>
+                <p>+ de 120</p>
+              </div>
+
+              <div class="mini-card text-gradient">
+                <h2>Exportações Brl</h2>
+                <p>+19 milh</p>
+              </div>
+            </div>
+              </div>
+
+            <div class="hero-image">
+              <img src="https://th.bing.com/th/id/R.f6d7c4e7449a04230a1bf0fecd457ac7?rik=iu54Q6EGJuD5zA&pid=ImgRaw&r=0" alt="xícara de café">
+               <div class="eventBackground"></div>
+            </div>
+          </div>
+      </div>
+    </section>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+
+      <!--- eu amanheçho pensando em ti, 
+      eu anoiteço pensando em ti [música]--->
+
+
+
+ 
+    
+  </body>
+</html>
